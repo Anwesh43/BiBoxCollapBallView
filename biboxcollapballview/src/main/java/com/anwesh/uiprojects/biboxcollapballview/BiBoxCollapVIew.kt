e936b76570d5f9cacb11f085b629ab71da3fd9c3
Anwesh43 <anweshthecool0@gmail.com>
2020-05-08 05:50:24 +0530

    adding constants and libraries

--- biboxcollapballview/src/main/java/com/anwesh/uiprojects/biboxcollapballview/BiBoxCollapVIew.kt
+++ biboxcollapballview/src/main/java/com/anwesh/uiprojects/biboxcollapballview/BiBoxCollapVIew.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.biboxcollapballview
 /**
  * Created by anweshmishra on 08/05/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val nodes : Int = 5
+val lines : Int = 2
+val scGap : Float = 0.02f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
