7dae35ce4c5baddd0f658e81cc69c78ecf418650
Anwesh43 <anweshthecool0@gmail.com>
2020-05-08 06:14:26 +0530

    adding BBCNode class

--- biboxcollapballview/src/main/java/com/anwesh/uiprojects/biboxcollapballview/BiBoxCollapVIew.kt
+++ biboxcollapballview/src/main/java/com/anwesh/uiprojects/biboxcollapballview/BiBoxCollapVIew.kt
@@ -128,4 +128,47 @@ class BiBoxCollapView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BBCNode(var i : Int, val state : State = State()) {
+
+        private var next : BBCNode? = null
+        private var prev : BBCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = BBCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBCCNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BBCNode {
+            var curr : BBCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+
+    }
 }
