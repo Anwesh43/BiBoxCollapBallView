d3e32eef1e6d477be76a70d42d0438adf6a0791e
Anwesh43 <anweshthecool0@gmail.com>
2020-05-08 06:06:40 +0530

    adding BoiBoxCollapView class

--- biboxcollapballview/src/main/java/com/anwesh/uiprojects/biboxcollapballview/BiBoxCollapVIew.kt
+++ biboxcollapballview/src/main/java/com/anwesh/uiprojects/biboxcollapballview/BiBoxCollapVIew.kt
@@ -65,3 +65,19 @@ fun Canvas.drawBCCNode(i : Int, scale : Float, paint : Paint) {
     drawBiCollapCircle(scale, w, size, paint)
     restore()
 }
+
+class BiBoxCollapView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
