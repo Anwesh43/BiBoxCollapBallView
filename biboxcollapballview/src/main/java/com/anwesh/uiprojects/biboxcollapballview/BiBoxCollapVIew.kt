a6351149afbe665f6b37131193bf09ad5269bb33
Anwesh43 <anweshthecool0@gmail.com>
2020-05-08 06:16:20 +0530

    adding BiBoxCollap class

--- biboxcollapballview/src/main/java/com/anwesh/uiprojects/biboxcollapballview/BiBoxCollapVIew.kt
+++ biboxcollapballview/src/main/java/com/anwesh/uiprojects/biboxcollapballview/BiBoxCollapVIew.kt
@@ -169,6 +169,29 @@ class BiBoxCollapView(ctx : Context) : View(ctx) {
             cb()
             return this
         }
+    }
 
+    data class BiBoxCollap(var i : Int) {
+
+        private val root : BBCNode = BBCNode(0)
+        private var curr : BBCNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
     }
 }
